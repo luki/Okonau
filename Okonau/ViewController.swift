63501a2eae9d892699cb9053a3bee28dd0f12d8c
lukasamueller@icloud.com <millennium1412@yahoo.co.jp>
2016-04-14 15:28:11 +0200

    Initial Commit

---
+++ Okonau/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Okonau
+//
+//  Created by Lukas A. Müller on 14/04/16.
+//  Copyright © 2016 Lukas A. Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
